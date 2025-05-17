cf6b953372164c7f6483fd9d9309c7fd1c33a05e
Atharva Keshre <atharvakeshre@gmail.com>
2025-05-17 19:58:50 -0400

    Ssuper super final

--- backend/start.sh
+++ backend/start.sh
@@ -2,5 +2,6 @@
 
 pkill -f 'java -jar' || echo "Nothing to kill."
 sleep 1
+bash mv /home/ec2-user/dailysync/backend/.env /home/ec2-user/dailysync/backend/target/.env
 bash java -jar /home/ec2-user/dailysync/backend/target/DailySync-0.0.1-SNAPSHOT.jar > /home/ec2-user/app.log 2>&1 &
 echo "App started."
