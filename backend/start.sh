9aed1c4654c17efac6043a21ca69ac9482b1ad01
Atharva Keshre <atharvakeshre@gmail.com>
2025-05-17 18:37:49 -0400

    Super final

--- backend/start.sh
+++ backend/start.sh
@@ -2,5 +2,5 @@
 
 pkill -f 'java -jar' || echo "Nothing to kill."
 sleep 1
-nohup java -jar /home/ec2-user/dailysync/app.jar > /home/ec2-user/app.log 2>&1 &
+nohup java -jar /home/ec2-user/dailysync/app.jar/backend/target/DailySync-0.0.1-SNAPSHOT.jar > /home/ec2-user/app.log 2>&1 &
 echo "App started."
